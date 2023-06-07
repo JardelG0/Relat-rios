9699912cfe754a70a945eb7b8667757d9f95dc4e
Acer_Gameleira <jardelrlsjs@gmail.com>
2023-06-07 14:42:19 -0300

    So roda se tiver ~ na frente e adicionado msg de pronto e se ja estava pronto antes

--- Clear_INFORMATIVOS.py
+++ Clear_INFORMATIVOS.py
@@ -1,18 +1,25 @@
 import pandas as pd
 
-arquivo_excel_5 = "INFORMATIVO 5° GRE.xlsx"
-inf_5 = pd.read_excel(arquivo_excel_5, sheet_name="Sheet1")
+# Rodando o informativo da 5° GRE
+inf_5 = pd.read_excel("INFORMATIVO 5° GRE.xlsx", sheet_name="Sheet1")
 
-for i in range(len(inf_5)):
-    inf_5.loc[i, 'DIAS'] = inf_5['DIAS'][i][2:]
-inf_5.to_excel('INFORMATIVO 5° GRE.xlsx', index=False)
+try:
+    for i in range(len(inf_5)):
+        inf_5.loc[i, 'DIAS'] = inf_5['DIAS'][i][2:]
+    inf_5.to_excel('INFORMATIVO 5° GRE.xlsx', index=False)
+    print("\n\t ! INFORMATIVO 5° GRE ready !")
+except:
+    print('\n\t! INFORMATIVO 5° GRE already updated !\n')
 
 
-arquivo_excel_18 = "INFORMATIVO 18° GRE.xlsx"
-inf_18 = pd.read_excel(arquivo_excel_18, sheet_name="Sheet1")
 
-for i in range(len(inf_18)):
-    inf_18.loc[i, 'DIAS'] = inf_18['DIAS'][i][2:]
-inf_18.to_excel('INFORMATIVO 18° GRE.xlsx', index=False)
+# Rodando o informativo da 18° GRE
+inf_18 = pd.read_excel("INFORMATIVO 18° GRE.xlsx", sheet_name="Sheet1")
 
-print('\n\t! PRONTINHO !\n')
+try:
+    for i in range(len(inf_18)):
+        inf_18.loc[i, 'DIAS'] = inf_18['DIAS'][i][2:]
+    inf_18.to_excel('INFORMATIVO 18° GRE.xlsx', index=False)
+    print("\n\t ! INFORMATIVO 18° GRE ready !")
+except:
+    print('\n\t! INFORMATIVO 18° GRE already updated !\n')
