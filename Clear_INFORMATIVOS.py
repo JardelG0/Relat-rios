85c83669e1f63fd9ef28feeaa2b21ad3993c7733
Jardel_Gameleira <jardelrlsjs@gmal.com>
2023-07-01 20:58:08 -0300

    limpando vazios

--- Clear_INFORMATIVOS.py
+++ Clear_INFORMATIVOS.py
@@ -9,11 +9,18 @@ def main():
             try:
                 inf_5 = pd.read_excel("INFORMATIVO 5° GRE.xlsx", sheet_name="Sheet1")
                 try:
-                    for i in range(len(inf_5)):
+                    cont_5 = len(inf_5)
+                    for i in range(cont_5):
                         inf_5.loc[i, 'DIAS'] = inf_5['DIAS'][i][2:]
+
+                    inf_5 = inf_5.T
+                    for i in range(cont_5):
+                        if inf_5[i][1] == "":
+                            inf_5.pop(i)
+                    inf_5 = inf_5.T
+
                     inf_5.to_excel('INFORMATIVO 5° GRE.xlsx', index=False)
                     print("\n\t ! INFORMATIVO 5° GRE ready !")
-
                 except:
                     print('\n\t! INFORMATIVO 5° GRE already updated !\n')
             except:
@@ -24,8 +31,16 @@ def main():
             try:
                 inf_16 = pd.read_excel("INFORMATIVO 16° GRE.xlsx", sheet_name="Sheet1")
                 try:
-                    for i in range(len(inf_16)):
+                    cont_16 = len(inf_16)
+                    for i in range(cont_16):
                         inf_16.loc[i, 'DIAS'] = inf_16['DIAS'][i][2:]
+                    
+                    inf_16 = inf_16.T
+                    for i in range(cont_16):
+                        if inf_16[i][1] == "":
+                            inf_16.pop(i)
+                    inf_16 = inf_16.T
+
                     inf_16.to_excel('INFORMATIVO 16° GRE.xlsx', index=False)
                     print("\n\t ! INFORMATIVO 16° GRE ready !")
                 except:
@@ -38,8 +53,16 @@ def main():
             try:
                 inf_18 = pd.read_excel("INFORMATIVO 18° GRE.xlsx", sheet_name="Sheet1")
                 try:
-                    for i in range(len(inf_18)):
+                    cont_18 = len(inf_18)
+                    for i in range(cont_18):
                         inf_18.loc[i, 'DIAS'] = inf_18['DIAS'][i][2:]
+                    
+                    inf_18 = inf_18.T
+                    for i in range(cont_18):
+                        if inf_18[i][1] == "":
+                            inf_18.pop(i)
+                    inf_18 = inf_18.T
+
                     inf_18.to_excel('INFORMATIVO 18° GRE.xlsx', index=False)
                     print("\n\t ! INFORMATIVO 18° GRE ready !")
                     perm = False
